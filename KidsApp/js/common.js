168cfa185e6b01dc4fddb2d2da838c36755934dc
ThevinuDan <thevinusenaratne@outlook.com>
2024-01-16 14:36:47 +0530

    Update common.js

--- KidsApp/js/common.js
+++ KidsApp/js/common.js
@@ -10,6 +10,9 @@ function changePage(page) {
 function goToChapter(chapter) {
   window.location.href = "KidsApp/html/" + chapter + ".html";
 }
+function goToHome() {
+  window.location.href = "index.html";
+}
 document.getElementById("page1").style.display = "flex";
 
 // button click sound
